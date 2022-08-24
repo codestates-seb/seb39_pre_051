6bf0abe2f4c7efe22ac2c5fba000ed81f04b199d
soobinkim-kor <shb03207@naver.com>
2022-08-25 00:22:16 +0900

    BE initial commit

---
+++ server/pre51/src/main/java/com/codestates/pre51/Pre51Application.java
@@ -0,0 +1,13 @@
+package com.codestates.pre51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre51Application.class, args);
+	}
+
+}
---
+++ server/pre51/src/test/java/com/codestates/pre51/Pre51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.pre51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pre51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
