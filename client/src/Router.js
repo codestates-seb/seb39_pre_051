61a9cd5820953a6ffd1f0b6d169c5ebc1a3eb3d4
LeeChangHoon <changhoon1030@gmail.com>
2022-08-24 20:00:02 +0900

    FE initial setting

---
+++ client/src/Router.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./page/Home"
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" element ={<Home />} />
+        </Routes>
+    )
+}
+
+export default Router
