1e598a80e793f64781de73830041df1cecbe8670
soobinkim-kor <shb03207@naver.com>
2022-08-25 16:25:15 +0900

    Add : Question Controller, Service, ServiceImpl
    Question 도메인 패키지들 추가

--- server/pre51/src/main/java/com/codestates/pre51/question/controller/QuestionController.java
+++ server/pre51/src/main/java/com/codestates/pre51/question/controller/QuestionController.java
@@ -2,26 +2,37 @@ package com.codestates.pre51.question.controller;
 
 import com.codestates.pre51.question.entity.Question;
 import com.codestates.pre51.question.repository.QuestionRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codestates.pre51.question.service.QuestionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/questions")
 public class QuestionController {
     private final QuestionRepository questionRepository;
-
-    public QuestionController(QuestionRepository questionRepository) {
+    private final QuestionService questionService;
+    public QuestionController(QuestionRepository questionRepository, QuestionService questionService) {
         this.questionRepository = questionRepository;
+        this.questionService=questionService;
     }
 
-    @GetMapping("/questions")
-    public String getQuestions(){
-        return "hello world";
+    @GetMapping("")
+    public List<Question> getQuestions(){
+        return questionRepository.findAll();
     }
 
-    @PostMapping("/signup")
+    @GetMapping("/page")
+    public Page<Question> getPagedQuestions(@RequestParam Integer page, Integer size){
+        
+        return questionService.findQuestionsByPageRequest(page,size);
+    }
+
+    @PostMapping("/ask")
     public Question signUp(){
         final Question question = Question.builder().question_id(0).email("test@test.com").build();
         System.out.println(question.getQuestion_id());
