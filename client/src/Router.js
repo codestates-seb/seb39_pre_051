a4af3187be4b6daad5b96bf9765f315aee2ff429
jungyunhan <hanjy2008@naver.com>
2022-08-29 11:23:52 +0900

    Fix : Router and TopBar

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Ask from './page/Ask';
 import Home from './page/Home';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/ask' element={<Ask />} />
     </Routes>
   );
 };
