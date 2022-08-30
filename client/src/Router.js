3cc71ddffbe0eb4e98459988070419e28536d5d0
jungyunhan <hanjy2008@naver.com>
2022-08-30 09:23:53 +0900

    Add : UserProfile Fix : TopBar, Router, Ask

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Ask from './page/Ask';
 import Home from './page/Home';
+import Login from './page/Login';
+import SignUp from './page/SignUp';
+import UserProfile from './page/UserProfile';
 
 const Router = () => {
   return (
@@ -9,6 +12,7 @@ const Router = () => {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='/ask' element={<Ask />} />
+      <Route path='/users' element={<UserProfile />} />
     </Routes>
   );
 };
