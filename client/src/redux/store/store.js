29c6cda203dc6897b8c728c44fb742c3bf1b5a6d
LeeChangHoon <changhoon1030@gmail.com>
2022-08-31 17:40:38 +0900

    Feat : isLoggedIn 상태에 따른 접근 관리

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeSlice from '../slice/themeSlice';
+import userInfoSlice  from '../slice/useInfoSlice';
+
 export const store = configureStore({
   reducer: {
-    themeSlice
+    themeSlice,
+    userInfoSlice
   },
 });
