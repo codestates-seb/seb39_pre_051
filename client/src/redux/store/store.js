428c75f1e7846368a98a992a4a83a8ef8c0462de
LeeChangHoon <changhoon1030@gmail.com>
2022-09-02 09:22:25 +0900

    Feat : 답변 작성 기능 추가

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import questionSlice from '../slice/questionSlice';
 import themeSlice from '../slice/themeSlice';
-import userInfoSlice  from '../slice/useInfoSlice';
+import userInfoSlice  from '../slice/userInfoSlice';
 
 export const store = configureStore({
   reducer: {
     themeSlice,
-    userInfoSlice
+    userInfoSlice,
+    questionSlice
   },
 });
