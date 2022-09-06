afcf98f80ff58f0b8a112813f0730108f5417be3
jungyunhan <hanjy2008@naver.com>
2022-09-06 10:50:10 +0900

    Fix : 다크 모드 수정

--- client/src/component/BestAnswerMark.js
+++ client/src/component/BestAnswerMark.js
@@ -1,63 +1,67 @@
 import styled from 'styled-components';
 import axios from 'axios';
-import {useDispatch} from'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { readQuestion } from '../redux/slice/questionSlice';
 
-
 const BestAnswerMark = (props) => {
+  const themeState = useSelector((state) => state.themeSlice).theme;
   const dispatch = useDispatch();
-  const handleBestAnswer = async() => {
-    console.log('hello')
-    console.log(props.questionId)
-    await axios.patch(`/questions/${props.questionId}/${props.id}`)
-    .then((res)=>console.log(res))
-    dispatch(readQuestion(props.questionId))
-  }
-  const handleResetBestAnswer = async() => {
-    console.log('hello')
-    console.log(props.questionId)
-    await axios.patch(`/questions/${props.questionId}/0`)
-    .then((res)=>console.log(res))
-    dispatch(readQuestion(props.questionId))
-  }
+  const handleBestAnswer = async () => {
+    console.log('hello');
+    console.log(props.questionId);
+    await axios
+      .patch(`/questions/${props.questionId}/${props.id}`)
+      .then((res) => console.log(res));
+    dispatch(readQuestion(props.questionId));
+  };
+  const handleResetBestAnswer = async () => {
+    console.log('hello');
+    console.log(props.questionId);
+    await axios
+      .patch(`/questions/${props.questionId}/0`)
+      .then((res) => console.log(res));
+    dispatch(readQuestion(props.questionId));
+  };
 
-  return(
-    props.isBestAnswer ? (
+  return props.isBestAnswer ? (
     props.isQuestionWriter ? (
-      <CheckMarkWrapper onClick={()=>handleResetBestAnswer()}>
-      <svg width='36' height='36' viewBox='0 0 36 36' className='bestAnswer' >
-        <path d='m6 14 8 8L30 6v8L14 30l-8-8v-8Z'></path>
-      </svg>
-    </CheckMarkWrapper>
+      <CheckMarkWrapper
+        themeState={themeState}
+        onClick={() => handleResetBestAnswer()}
+      >
+        <svg width='36' height='36' viewBox='0 0 36 36' className='bestAnswer'>
+          <path d='m6 14 8 8L30 6v8L14 30l-8-8v-8Z'></path>
+        </svg>
+      </CheckMarkWrapper>
     ) : (
-      <CheckMarkWrapper>
-      <svg width='36' height='36' viewBox='0 0 36 36' className='bestAnswer' >
+      <CheckMarkWrapper themeState={themeState}>
+        <svg width='36' height='36' viewBox='0 0 36 36' className='bestAnswer'>
+          <path d='m6 14 8 8L30 6v8L14 30l-8-8v-8Z'></path>
+        </svg>
+      </CheckMarkWrapper>
+    )
+  ) : props.isQuestionWriter ? (
+    <CheckMarkWrapper
+      themeState={themeState}
+      onClick={() => handleBestAnswer()}
+    >
+      <svg width='36' height='36' viewBox='0 0 36 36' className='answer'>
         <path d='m6 14 8 8L30 6v8L14 30l-8-8v-8Z'></path>
       </svg>
     </CheckMarkWrapper>
-    )
-  ) : 
-  props.isQuestionWriter ? (
-    <CheckMarkWrapper onClick={()=>handleBestAnswer()}>
-    <svg width='36' height='36' viewBox='0 0 36 36' className='answer' >
-      <path d='m6 14 8 8L30 6v8L14 30l-8-8v-8Z'></path>
-    </svg>
-  </CheckMarkWrapper>
   ) : (
-    <>
-    </>
-  )
-  )
+    <></>
+  );
 };
 
 const CheckMarkWrapper = styled.div`
   .bestAnswer {
-    cursor: ${(props)=>props.isQuestionWriter ? "pointer" : ""};
-    fill: #48A868;
+    cursor: ${(props) => (props.isQuestionWriter ? 'pointer' : '')};
+    fill: ${(props) => (props.themeState === 'light' ? '#48a868' : '#62b47d')};
   }
   .answer {
     cursor: pointer;
-    fill: #2d2d2d;
+    fill: ${(props) => (props.themeState === 'light' ? '#2d2d2d' : '#696f75')};
   }
 `;
 
